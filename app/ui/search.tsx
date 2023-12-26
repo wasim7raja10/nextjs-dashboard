3740c82d9d1c9d3bbce4c8cbfbc3c2c3c5f47ce9
Wasim Raja <wasim7raja10@gmail.com>
2023-12-26 20:01:40 +0530

    Refactor search functionality and add pagination

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -10,9 +10,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const { replace } = useRouter();
 
   const handleSearch = useDebouncedCallback((term: string) => {
-    console.log('searching', term);
-
     const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
     if (term) {
       params.set('query', term);
     } else {
@@ -20,7 +19,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
     }
     replace(`${pathname}?${params.toString()}`);
   }, 500);
-  
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
