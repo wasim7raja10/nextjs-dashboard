d60b68f94eb2970aee180f6cd8d2ee494193e2c0
Wasim Raja <wasim7raja10@gmail.com>
2023-12-26 19:35:47 +0530

    Refactor invoices page and search component

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,14 +1,30 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 
 export default function Search({ placeholder }: { placeholder: string }) {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  function handleSearch(term: string) {
+    const params = new URLSearchParams(searchParams);
+    if (term) {
+      params.set('query', term);
+    } else {
+      params.delete('query');
+    }
+    replace(`${pathname}?${params.toString()}`);
+  }
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
         Search
       </label>
       <input
+        onChange={(e) => handleSearch(e.currentTarget.value)}
+        defaultValue={searchParams.get('query')?.toString()}
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
       />
